eaa76a19d7809bc75d39c137efc7a9a43f493fb7
jozel <jozeluin@gmail.com>
2024-03-19 12:46:23 +0100

    Se comienza el Scaffold en CityApp , se coloca el appbar y ahora estoy con la navegacion

--- app/src/main/java/com/cursokotlin/mycity/ui/CityApp.kt
+++ app/src/main/java/com/cursokotlin/mycity/ui/CityApp.kt
@@ -1,10 +1,12 @@
 package com.cursokotlin.mycity.ui
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
@@ -15,12 +17,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.cursokotlin.mycity.model.TypePlaceScreen
 import com.cursokotlin.mycity.ui.utils.CityContentType
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CityApp(
     windowSize: WindowWidthSizeClass,
@@ -56,6 +60,36 @@ fun CityApp(
         }
 
     }
+
+
+    Scaffold(
+        topBar = {
+            CityAppBar(
+                typePlaceScreen = curresntScreen,
+                canNavigateBack = navController.previousBackStackEntry!=null,
+                navigateup = { navController.navigateUp() })
+
+        }
+    ) { innerPadding->
+
+        NavHost(
+            navController = navController,
+            startDestination = TypePlaceScreen.Cafeteria.name,
+            modifier = Modifier.padding(innerPadding)
+        ){
+
+
+
+
+        }
+    }
+
+
+
+
+
+
+
 
 
 }
